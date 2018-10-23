3873b4bb46f0ae644dc630f125fef369cb2230e1
Radovan Sroka <rsroka@redhat.com>
2018-10-23 12:06:46 +0200

    Added warning filter

--- src/lex/Lex.cpp
+++ src/lex/Lex.cpp
@@ -3,7 +3,7 @@
 namespace TPJparser {
 
     Lex::Lex(std::istream& stream) : _stream(stream) {}
-    Token& Lex::getToken() {}
-    void Lex::ungetToken(Token& t) {}
+//    Token& Lex::getToken() {}
+//    void Lex::ungetToken(Token& t) {}
 
 }
