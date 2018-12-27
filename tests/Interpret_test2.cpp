9ba5d8a7533986a9702fb5607aeaaf095127f846
Radovan Sroka <rsroka@redhat.com>
2018-12-27 16:07:46 +0100

    Literal operations

---
+++ tests/Interpret_test2.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "syntax/Syntax.hpp"
+
+using namespace TPJparser;
+
+int main()
+{
+    long result = 0;
+    {
+        std::string inputProgram ("3+2");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("3*2");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("3.0/2");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("2 && 3");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("2 || 3");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("3*2-4");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("3*(2-4)");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("3 == 5");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("!1");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("!1.0");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+
+    {
+        std::string inputProgram ("-1");
+        std::istringstream stream(inputProgram);
+        Syntax syntax(stream);
+        result += syntax.ParseExpression();
+        if (result) return result;
+        syntax.getIntepreter().append(Interpret::Instructions::PRINT);
+        result += syntax.getIntepreter().run();
+    }
+   return result;
+}
