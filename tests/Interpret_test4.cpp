54154c70985d2e1751a1f1e5fc75e8f0eaa9e8a7
Radovan Sroka <rsroka@redhat.com>
2018-12-28 18:23:24 +0100

    Generate assignment

--- tests/Interpret_test4.cpp
+++ tests/Interpret_test4.cpp
@@ -30,5 +30,53 @@ int main()
         if (nonstd::get<long>(res._value) != 4) result++;
     }
 
+    {
+        std::string program = "int main(){int x = 4; int y = x + 2;}";
+        std::istringstream ss(program);
+        Syntax s(ss);
+
+        s.Parse();
+        s.getIntepreter().useVariable(1);
+        s.getIntepreter().run();
+        const auto  res = s.getIntepreter().getStack().back();
+        if (res._type != SymbolTableItem::Type::INT) result++;
+        if (nonstd::get<long>(res._value) != 6) result++;
+
+        s.getIntepreter().append(Interpret::Instructions::PRINT);
+        s.getIntepreter().run();
+    }
+
+    {
+        std::string program = "int main(){int x = 4; x = 5;}";
+        std::istringstream ss(program);
+        Syntax s(ss);
+
+        s.Parse();
+        s.getIntepreter().useVariable(0);
+        s.getIntepreter().run();
+        const auto  res = s.getIntepreter().getStack().back();
+        if (res._type != SymbolTableItem::Type::INT) result++;
+        if (nonstd::get<long>(res._value) != 5) result++;
+
+        s.getIntepreter().append(Interpret::Instructions::PRINT);
+        s.getIntepreter().run();
+    }
+
+    {
+        std::string program = "int main(){int x = 4; x = 5;}";
+        std::istringstream ss(program);
+        Syntax s(ss);
+
+        s.Parse();
+        s.getIntepreter().useVariable(0);
+        s.getIntepreter().run();
+        const auto  res = s.getIntepreter().getStack().back();
+        if (res._type != SymbolTableItem::Type::INT) result++;
+        if (nonstd::get<long>(res._value) != 5) result++;
+
+        s.getIntepreter().append(Interpret::Instructions::PRINT);
+        s.getIntepreter().run();
+    }
+
    return result;
 }
