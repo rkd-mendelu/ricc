cc1d7272819fa3ca5820c6faec1ee04a8ee7835d
Radovan Sroka <rsroka@redhat.com>
2019-04-17 17:13:08 +0200

    Added while support

---
+++ tests/Interpret_test7_while.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "syntax/Syntax.hpp"
+
+using namespace TPJparser;
+
+int main()
+{
+    long result = 0;
+
+    {
+      std::string program = "while(false) {printSTRING(\"in WHILE\");} printSTRING(\"out WHILE\");";
+      std::cout << program << std::endl;
+      std::istringstream ss(program);
+      Syntax s(ss);
+
+      result += s.Parse();
+      s.getIntepreter().printCode();
+      result += s.getIntepreter().run();
+    }
+
+    {
+      std::string program = "int x = 0; while(x < 5) {printINT(x); x = x + 1;} printSTRING(\"out WHILE\");";
+      std::cout << program << std::endl;
+      std::istringstream ss(program);
+      Syntax s(ss);
+
+      result += s.Parse();
+      s.getIntepreter().printCode();
+      result += s.getIntepreter().run();
+    }
+
+    return result;
+}
