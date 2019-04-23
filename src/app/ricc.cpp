5c052403f7d1433a2dc0b608b7ebe8381117649b
Radovan Sroka <rsroka@redhat.com>
2019-04-23 13:13:30 +0200

    Added json submodule

--- src/app/ricc.cpp
+++ src/app/ricc.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
+#include "nlohmann/json.hpp"
 
-int main() { return 0; }
+using namespace nlohmann;
+
+int main() {
+  // create object from string literal
+  json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
+
+  // or even nicer with a raw string literal
+  auto j2 = R"(
+  {
+    "happy": true,
+    "pi": 3.141
+  }
+  )"_json;
+
+  std::cout << j2.dump(4) << std::endl;
+  return 0;
+}
