3d9c3cb547ee276f8f3f60c97ce0dc5d369c1862
Tibor Dudlák <tdudlak@redhat.com>
2019-04-25 13:21:27 +0200

    Reformat comments

--- tests/library/sem_test1.cpp
+++ tests/library/sem_test1.cpp
@@ -15,42 +15,47 @@ std::map<std::string, std::pair<int, int> > tests{
     /* input value, return, testnumber */
 
     {"string foo(); int bar (int bar) {int baz; foo(bar,baz);}",
+     //           ⬑--------- missing func body
      {SYNTAX_ERROR, 1}},
-    //            ⬑------------------------------------------------------┛
-    //            missing func body
-    //------------->{"string foo(){} int bar (int bar) {int baz;
-    // foo(bar,baz);}", {SEMANTICS_ERROR, 2}},
-    //                                                ⬑--------------------┛ too
-    //                                                many params
+
+    {"string foo(){} int bar (int bar) {int baz; foo(bar,baz);}",
+     //                                           ⬑-- too many params
+     {SEMANTICS_ERROR, 2}},
+
     {"int foo(int a, float b){} int bar (int bag) {float baz; foo(bag, baz);}",
      {RET_OK, 3}},  // TODO void not working
-    // {"int add(float a, float b){return a + b;} int bar (int bag) {float lol,
-    // sad ; add(bag, sad); return lol;}", {SEMANTICS_ERROR, 4}}, // FIXME
-    // should be SEMANTICS_ERROR TODO ParseExpression(expType)
-    // // ⬑---------------------------------┛ missmatch int/float
-    //---------------> {"int global(){} for(int zidan;( zidan <  motorko );
-    // zidan = zidan + 1 ){print(9);}", {SEMANTICS_ERROR, 5}},
-    //                                                                        ⬑-------------------┛
-    //                                                                        print
-    //                                                                        not
-    //                                                                        defined
-    //---------------->{"int global(){} for(int zidan;( zidan <  motorko );
-    // zidan = zidan + 1 ){global(zidan);}", {SEMANTICS_ERROR, 6}},
-    //                                                                               ⬑-------------------┛ mismatch argc
+
+    // {"int add(float a, float b){return a + b;} int bar (int bag) "
+    //  "{float lol, sad ; add(bag, sad); return lol;}", {SEMANTICS_ERROR, 4}},
+    //                   ⬑-------- missmatch int/float
+    // should be SEMANTICS_ERROR maybe TODO ParseExpression(expType)
+    // FIXME
+
+    // {"int global(){} for(int zidan;( zidan <  motorko );"
+    // //                                           ⬑-- not defined
+    //  "zidan = zidan + 1 ){print(9);}", {SEMANTICS_ERROR, 5}},
+
+    // {"int global(){} for(int zidan;( zidan <  motorko );"
+    //  " zidan = zidan + 1 ){global(zidan);}", {SEMANTICS_ERROR, 6}},
+    // //                              ⬑------ mismatch argc
+
     {"string }", {SYNTAX_ERROR, 7}},
     {"string a;", {RET_OK, 8}},
     {"string aa; string ret(string a ) {return a + aa;}", {RET_OK, 8}},
-    //------------------->{"string a; ret(string a) {return a;}",
-    //{SEMANTICS_ERROR, 9}},
-    //           ⬑--------------------------------┛ ret not defined
+
+    // {"string a; ret(string a) {return a;}",
+    //  //          ⬑---- ret not defined
+    //  {SEMANTICS_ERROR, 9}},
+
     {"", {RET_OK, 10}},  // Note empty input
 
     {"int foo(int a, float b){} void bar (int bag) {float baz; foo(bag, baz); "
      "return;}",
      {RET_OK, 11}},
+
     // {"int global(){} for(int zidan;( zidan <  motorko ); zidan = zidan + 1
     // ){global(9);}", {SEMANTICS_ERROR, 12}},
-    // // ⬑-------------------┛ mismatch argc // FIXME 9 should not pass
+    //  // ⬑-------------------┛ mismatch argc // FIXME 9 should not pass
 
 };
 
